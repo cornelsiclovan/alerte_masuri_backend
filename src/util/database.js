efc6d4f154631344dd8b4ebee81a9cc8e098e0b5
cornel <cornel.siclovan@gmail.com>
2023-09-27 15:06:26 +0300

    test

--- src/util/database.js
+++ src/util/database.js
@@ -1,17 +1,18 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("alert_ar_sech_cj", "root", "", 
-{
-  dialect: "mysql",
-  host: "localhost",
-  timezone: '+00:00',
-}, {
-  charset: 'utf8',
-  collate: 'utf8_unicode_ci'
-}
-
+const sequelize = new Sequelize(
+  "alert_ar_sech_cj",
+  "root",
+  "",
+  {
+    dialect: "mysql",
+    host: "localhost",
+    timezone: "+00:00",
+  },
+  {
+    charset: "utf8",
+    collate: "utf8_unicode_ci",
+  }
 );
-
-
 
 module.exports = sequelize;
